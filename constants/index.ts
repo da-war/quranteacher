546c6ea90b38a6b5c698b9aa5a21ce34e6edb2c3
Rana Dawar Abdullah <ranadawarabdullah@Dawar.local>
2024-08-29 18:37:09 +0500

    json compressed

--- constants/index.ts
+++ constants/index.ts
@@ -24,7 +24,6 @@ import star from "@/assets/icons/star.png";
 import target from "@/assets/icons/target.png";
 import to from "@/assets/icons/to.png";
 import check from "@/assets/images/check.png";
-import getStarted from "@/assets/images/get-started.png";
 import message from "@/assets/images/message.png";
 import noResult from "@/assets/images/no-result.png";
 import onboarding1 from "@/assets/images/onboarding1.png";
@@ -40,7 +39,6 @@ export const images = {
   onboarding1,
   onboarding2,
   onboarding3,
-  getStarted,
   signUpCar,
   check,
   noResult,
